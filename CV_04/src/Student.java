fee03f24039a7cbfa56fa1662f250cfe192aa899
Tomáš Koláček <80208930+Tomkolda@users.noreply.github.com>
2022-03-03 08:05:28 +0100

    Update Student.java

--- CV_04/src/Student.java
+++ CV_04/src/Student.java
@@ -15,14 +15,14 @@ public class Student {
 	{
 		return rocnik;
 	}
-	
+	/**/
 	public float getStudijniPrumer() throws Vyjimky{
 		if (this.studijniPrumer == 0)
 			throw new Vyjimky();
 		else
 			return studijniPrumer;
 	}
-
+	/**/
 	public void setStudijniPrumer(float studijniPrumer) throws Vyjimky {
 		if (studijniPrumer >= 1 && studijniPrumer <= 5)
 			this.studijniPrumer = studijniPrumer;
